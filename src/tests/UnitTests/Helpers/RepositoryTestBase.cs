48225c0bf9236196e21afc7955d11a0070c1c743
chenqi_lscy <chenqilscy@users.noreply.github.com>
2026-03-14 13:16:30 +0800

    test: add RepositoryTestBase with SQLite in-memory database

---
+++ src/tests/UnitTests/Helpers/RepositoryTestBase.cs
@@ -0,0 +1,34 @@
+// src/tests/UnitTests/Helpers/RepositoryTestBase.cs
+using CKY.MultiAgentFramework.Repository.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace CKY.MultiAgentFramework.Tests.Helpers;
+
+public abstract class RepositoryTestBase : IAsyncLifetime
+{
+    protected MafDbContext DbContext { get; private set; } = null!;
+
+    public async Task InitializeAsync()
+    {
+        var options = new DbContextOptionsBuilder<MafDbContext>()
+            .UseSqlite("DataSource=:memory:")
+            .EnableSensitiveDataLogging()
+            .Options;
+
+        DbContext = new MafDbContext(options);
+        await DbContext.Database.OpenConnectionAsync();
+        await DbContext.Database.EnsureCreatedAsync();
+
+        await SeedTestDataAsync();
+    }
+
+    public async Task DisposeAsync()
+    {
+        await DbContext.Database.CloseConnectionAsync();
+        await DbContext.DisposeAsync();
+    }
+
+    protected virtual async Task SeedTestDataAsync()
+    {
+    }
+}
